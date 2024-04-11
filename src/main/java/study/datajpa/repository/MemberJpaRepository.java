d0ca0dbb4a8737912907ae988422700f854162af
Yeonjoo Lee <joos0207@naver.com>
2024-04-11 23:47:08 +0900

    순수 JPA 기반 리포지토리 만들기

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,17 +5,44 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJpaRepository {
 
     @PersistenceContext //엔티티 매니저를 가져옴
     private EntityManager em;
 
+    //등록
     public Member save(Member member) {
         em.persist(member);
         return member;
     }
 
+    //삭제
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    //모두 조회
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    //단건 조회 - Optional
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member); //null일 수 있다.
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class) //count가 Long타입으로 반환됨.
+                .getSingleResult();
+    }
+
+    //단건 조회
     public Member find(Long id) {
         return em.find(Member.class, id);
     }
--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -3,14 +3,17 @@ package study.datajpa.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional //스프링프레임워크의 트랜젝셔널로 선택 - 기능이 더 많다.
-//@Rollback(false) //롤백 기능 끄기 - 쿼리 확인 가능. db에도 반영된거 확인 가능. - 실무에서 테스트할 땐 롤백
+@Rollback(false) //롤백 기능 끄기 - 쿼리 확인 가능. db에도 반영된거 확인 가능. - 실무에서 테스트할 땐 롤백
 class MemberJpaRepositoryTest {
 
     @Autowired MemberJpaRepository memberJpaRepository;
@@ -25,5 +28,35 @@ class MemberJpaRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member); //같은 트랜잭션 안이면 비교했을 때 true를 보장한다.
+    }
+
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        //단건 조회 검증
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        findMember1.setUsername("member!!!!!"); //알아서 업데이트 쿼리가 나가서 db에도 반영된다. dirty checking(변경 감지)
+
+        /*//리스트 조회 검증
+        List<Member> all = memberJpaRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        //카운트 검증
+        long count = memberJpaRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        //삭제 검증
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+        long deletedCount = memberJpaRepository.count();
+        assertThat(deletedCount).isEqualTo(0);*/
     }
 }
